68fcb0e22ce45d2cdc8f3a9655839e9464d727b3
junki <junki@h-MacBook-Air-sl-junki.local>
2021-03-28 11:11:30 +0900

    3일차 공부

--- README.md
+++ README.md
@@ -45,6 +45,23 @@ Html 대응
 + 데이터 바인딩이 쉬워짐 -> 리엑트 최대 장점
 ### ₩ { 변수, 함수 등 } ₩
 
+# 3일차 강의
+
++ 데이터는 1.변수에 넣거나 2. state에 넣음
+
+    + state 만드는법
+        1. {ustState} 상단에 첨부
+        2. useState(데이터)
+    + state
+        1. 변수 대신 쓰는 데이터 저장공간
+        2. useState()를 이용해 만듦
+        3. 문자, 숫자, arraym object 다 가능함
+    + state 사용 이유.
+        1. web이 App처럼 동작하게 만들고 싶기때문
+        2. state가 변경되면, HTML이 자동으로 재렌더링이 됨.
+    + 자주 바뀌고 중요한 데이터는 State로 만듦
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts = '영등포 맛집'
-  function 함수() {
-    return 100
-  }
-  
+
+  // destructuring 문법 : ES6 신 문법
+  // 글제목 : state데이터 / 글제목변경 : state 변경함수
+  let [글제목, 글제목변경] = useState(['양말 추천', '고기 맛집', '초밥 맛집']);
+
+  let posts = '영등포 맛집';
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ { color : 'blue', fontSize : '30px' } }>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <h4> { posts } </h4>
-      <h4> { 함수() } </h4>
-      <img src={ logo } />
+      <div className="list">
+        <h3> { 글제목[0] } </h3>
+        <p>21.03.18일 1번 수행</p>
+        <hr/>
+        <h3> { 글제목[1] } </h3>
+        <p>21.03.18일 2번 수행</p>
+        <hr/>
+        <h3> { 글제목[2] } </h3>
+        <p>21.03.18일 3번 수행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
