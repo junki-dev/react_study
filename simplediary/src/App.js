69a0807a205de5943cdc69b5da400db35aa6f256
junki.kim <kjgcde@gmail.com>
2022-06-09 11:14:03 +0900

    Feat: React 배열 사용하기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,10 +1,76 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "김준기",
+//     content: "하이 1",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "황혜진",
+//     content: "하이 2",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "송성현",
+//     content: "하이 3",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "박신웅",
+//     content: "하이 4",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
